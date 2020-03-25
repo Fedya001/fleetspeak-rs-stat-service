9963f75ab995316c2896ce34c4eb9dece7941e51
Fedya001 <rabbitera@gmail.com>
2020-03-25 21:40:35 +0300

    Add fleetspeak and prost dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+pub mod stat {
+    include!(concat!(env!("OUT_DIR"), "/fleetspeak.stat.rs"));
+}
+
 fn main() {
-    println!("Hello, world!");
+    fleetspeak::startup("0.0.1")
+        .expect("Failed to establish connection with Fleatspeak client");
 }
